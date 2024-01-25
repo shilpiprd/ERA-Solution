0bc842e57a7f1d77f1cc31224ff1a506d86a7cd9
shilpiprd <shilpiprd@gmail.com>
2024-01-26 01:15:26 +0530

    updating draft

--- Sess11 Solution/my_main.py
+++ Sess11 Solution/my_main.py
@@ -8,61 +8,24 @@ import torch.optim as optim
 from models.resnet import ResNet18, ResNet34
 from tqdm import tqdm 
 
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 import numpy as np
 from torchvision import datasets, transforms
 import cv2
-from my_utils import get_lr, visualize_misclassified_images, visualize_loss_accuracy
+from my_utils import get_lr, visualize_misclassified_images, visualize_loss_accuracy, test_loader, classes, train_loader, device
 import torchvision
 import matplotlib.pyplot as plt
 #for gradcam 
-import matplotlib.pyplot as plt
-from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
-from torchvision.models import resnet50
+# import matplotlib.pyplot as plt
+# from pytorch_grad_cam import GradCAM
+# from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+# from pytorch_grad_cam.utils.image import show_cam_on_image
+# from torchvision.models import resnet50
 
 
-cuda = torch.cuda.is_available()
-dataloader_args = dict(shuffle=True, batch_size=512, num_workers=4, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# Train Phase transformations
-albu_transforms = A.Compose([ #modified this slightly
-    A.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
-    A.PadIfNeeded(min_height=40, min_width=40, border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)),  # Padding by 4
-    A.RandomCrop(32, 32),  # Random Crop to 32x32
-    A.HorizontalFlip(p=0.5),  # FlipLR - Horizontal flip
-    A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=(0, 0, 0), p=0.5),  # CutOut
-    ToTensorV2(),
-])
-
-def albu_transform(image): 
-    # Convert PIL image to numpy array
-    image_np = np.array(image)
-    # Apply Albumentations transforms
-    transformed = albu_transforms(image=image_np)
-    return transformed['image']
-
-train = datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=albu_transform)
-# train dataloader
-train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
 
 
-# Test Phase transformations
-test_transforms = transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-                                       ])
-test = datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=test_transforms)
-# test dataloader
-test_loader = torch.utils.data.DataLoader(test, **dataloader_args)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+
 
 
 lr = 0.1 #(default)
@@ -85,8 +48,8 @@ def train(model, device= device, train_loader= train_loader, optimizer= optimize
     train_loss = 0
     correct = 0
     total = 0
-    train_acc = []     #adding 
-    train_losses = [] #adding
+    # train_acc = []     #adding 
+    # train_losses = [] #adding
     processed = 0     #adding
     for batch_idx, (data, targets) in enumerate(pbar):
         data, targets = data.to(device), targets.to(device)
@@ -112,8 +75,8 @@ def test(model, device, test_loader, criterion):
     test_loss = 0 
     correct = 0 
     total = 0 
-    test_losses = [] 
-    test_acc = [] 
+    # test_losses = [] 
+    # test_acc = [] 
     with torch.no_grad(): 
         for batch_idx, (data, targets) in enumerate(test_loader):
             data, targets = data.to(device), targets.to(device)
@@ -129,6 +92,7 @@ def test(model, device, test_loader, criterion):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    test_acc.append(100. * correct / len(test_loader.dataset))
     return test_acc, test_losses
 
 def visualize_train_data(): 
@@ -146,67 +110,3 @@ def visualize_train_data():
     imshow(torchvision.utils.make_grid(images[:4]))
     # print labels
     print(' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
-
-def visualize_gradcam_single(model, idx = 4): #pass index of image u wanna perform gradcam on and model.
-    dataiter = iter(test_loader)
-    images, labels = next(dataiter)
-    target_layers = [model.layer4[-1]]
-    input_tensor = images[idx].unsqueeze(dim=0)# Create an input tensor image for your model..
-    cam = GradCAM(model=model, target_layers=target_layers)
-    targets = None
-    grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
-    grayscale_cam = grayscale_cam[0, :]
-    mean = [0.4914, 0.4822, 0.4465]
-    std = [0.2023, 0.1994, 0.2010]
-
-    img = input_tensor.squeeze(0).to('cpu').numpy()
-    img = np.transpose(img, (1, 2, 0))  # Convert to numpy and reshape to HxWxC
-    img = std * img + mean
-    img = np.clip(img, 0, 1)
-    visualization = show_cam_on_image(img, grayscale_cam, use_rgb=True, image_weight=0.7)
-
-    fig, ax = plt.subplots(figsize=(2, 2))
-    ax.imshow(visualization)
-
-def gradcam_misclassified(model, device): 
-   #first get misclassified images
-    model.eval()
-    misclassified_images = [] 
-    actual_labels = [] 
-    actual_targets = [] 
-    predicted_labels = [] 
-
-    with torch.no_grad(): 
-        for data, target in test_loader: #target is the true label
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            _, pred = torch.max(output, 1) 
-            for i in range(len(pred)): 
-                if pred[1] != target[i]: 
-                        actual_targets.append(target[i]) 
-                        misclassified_images.append(data[i])
-                        actual_labels.append(classes[target[i]])
-                        predicted_labels.append(classes[pred[i]])
-    #GRADCAM 
-    target_layers = [model.layer4[-1]]
-    cam = GradCAM(model= model, target_layers = target_layers)
-    #plot the images 
-    fig = plt.figure(figsize=(12,5)) #talking about size of each image
-    for i in range(10): #we wana plot 10 images
-        sub = fig.add_subplot(2, 5, i+1)
-        input_tensor = misclassified_images[i].unsqueez(dim = 0)
-        targets = [ClassifierOutputTarget(actual_targets[i])] 
-        grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
-        grayscale_cam = grayscale_cam[0, :]
-        mean = [0.4914, 0.4822, 0.4465]
-        std = [0.2023, 0.1994, 0.2010]
-
-        img = input_tensor.squeeze(0).to('cpu').numpy() 
-        img = np.transpose(img, (1,2,0)) 
-        img = std* img + mean 
-        img = np.clip(img, 0, 1) 
-
-        visualization = show_cam_on_image(img, grayscale_cam, use_rgb=True, image_weight=0.7)
-        plt.imshow(visualization) 
-    plt.tight_layout(visualization)
-    plt.show() 
